a7081f33aef5cccc9c67921e1234b299b60b0ade
Lavanyakapoor <122595550+Lavanyakapoor@users.noreply.github.com>
2024-12-04 11:27:00 +0530

    Add files via upload

---
+++ EventMate/EventMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventMate
+//
+//  Created by Parul Singla on 04/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EventMate/EventMateTests/EventMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventMateTests.swift
+//  EventMateTests
+//
+//  Created by Parul Singla on 04/12/24.
+//
+
+import Testing
+@testable import EventMate
+
+struct EventMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
